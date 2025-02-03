1b45ce6ab58b7e7f2a9735750a2e9a0dae71f35f
ambe0001 <ambe0001@stud.kea.dk>
2025-02-03 14:53:37 +0100

    dynamisk index

--- produktliste.js
+++ produktliste.js
@@ -1,23 +1,28 @@
 console.log("script loaded...");
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const mycategory = urlParams.get("categories");
+
+console.log("The category is:", mycategory);
+
 let listContainer = document.querySelector(".produkt_liste_container");
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategory}`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
-function showList(products) {
-  console.log(products);
-  let markup = "";
-  products
-    .map((product) => {
-      markup += ` <div class="card">
+function showList(data) {
+  const markup = data
+    .map(
+      (product) =>
+        ` <div class="card">
                 <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="">
                 <h3>${product.productdisplayname}</h3>
                 <h4>${product.articletype}|${product.brandname}</h4>
                 <p>${product.price},-</p>
                 <a href="produkt.html">Read more</a>
-            </div>`;
-    })
+            </div>`
+    )
     .join("");
 
   console.log(markup);
